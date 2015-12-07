e2b4380b280d3f4e3b8034529be6f1c9903e14e0
mbyers2017 <mbyers@hmc.edu>
2015-12-07 15:44:11 -0800

    Attempted to add some tests. Not sure if they are working but it compiles and says all tests passed.

--- app/src/androidTest/java/maxbyers/capeventplanner/ApplicationTest.java
+++ app/src/androidTest/java/maxbyers/capeventplanner/ApplicationTest.java
@@ -3,11 +3,18 @@ package maxbyers.capeventplanner;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnitRunner;
+import android.test.ActivityInstrumentationTestCase2;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        // Adding a comment to test push.
     }
+
+
 }
---
+++ app/src/androidTest/java/maxbyers/capeventplanner/myTest.java
@@ -0,0 +1,45 @@
+package maxbyers.capeventplanner;
+
+/**
+ * Created by Max on 12/7/2015.
+ */
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnitRunner;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Before;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class myTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mActivity;
+
+    public myTest() {
+        super(MainActivity.class);
+        loginTest();
+
+        onView(withId(R.id.event_name_edittext))       // withId(R.id.my_view) is a ViewMatcher
+                .perform(click())               // click() is a ViewAction
+                .check(matches(isDisplayed())); // matches(isDisplayed()) is a ViewAssertion
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        mActivity = getActivity();
+    }
+
+    public void loginTest() {
+        onView(withId(R.id.login_button))       // withId(R.id.my_view) is a ViewMatcher
+                .perform(click())               // click() is a ViewAction
+                .check(matches(isDisplayed())); // matches(isDisplayed()) is a ViewAssertion
+    }
+}
